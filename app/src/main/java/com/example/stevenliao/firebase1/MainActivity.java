5208792f8c398206957f56a602579e2106bb5902
StevenLiao <slamslamslam12411@gmail.com>
2017-09-06 17:37:22 +0800

    9-6

--- app/src/main/java/com/example/stevenliao/firebase1/MainActivity.java
+++ app/src/main/java/com/example/stevenliao/firebase1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.stevenliao.firebase1;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseAuth.AuthStateListener authStateListener;
     private String userUID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +37,53 @@ public class MainActivity extends AppCompatActivity {
             }
 
             private void login() {
-                String email = ((EditText)findViewById(R.id.email)).getText().toString();
-                String password = ((EditText)findViewById(R.id.password)).getText().toString();
+                final String email = ((EditText)findViewById(R.id.email)).getText().toString();
+                final String password = ((EditText)findViewById(R.id.password)).getText().toString();
                 Log.d("AUTH", email+"/"+password);
-                auth.signInWithEmailAndPassword(email, password);
+                auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Log.d("onComplete", "登入失敗");
+                            register(email, password);
+                        }
+                    }
+                });
             }
+
+            private void register(final String email, final String password) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("登入問題")
+                        .setMessage("無此帳號，是否要以此帳號密碼註冊?")
+                        .setPositiveButton("註冊",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which){
+                                createUser(email, password);
+                                Log.d("onComplete", "create");
+                            }
+                        })
+                        .setNeutralButton("cancel", null)
+                        .show();
+            }
+
+            private void createUser(String email, String password) {
+                auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                String message = task.isComplete() ? "Success" : "Failed";
+                                new AlertDialog.Builder(MainActivity.this)
+                                        .setMessage(message)
+                                        .setPositiveButton("OK",null)
+                                        .show();
+                            }
+                        });
+
+            }
+
         });
 
         auth = FirebaseAuth.getInstance();
@@ -67,7 +115,5 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         auth.removeAuthStateListener(authStateListener);
     }
-
-
 
 }
